20ef1e340c3811ae6f1ff7c92d5ecf78d7a03c88
ash-shar <ashshar@cs.washington.edu>
2022-11-05 14:28:57 -0700

    added API

---
+++ api.py
@@ -0,0 +1,30 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+
+import json
+
+
+@app.route('/api/rewriting', methods=['POST'])
+def api_all():
+	if request.method == "POST": 
+		curr_post = request.json #request.args['SP']
+		# print(curr_post)
+
+		seeker_post = curr_post['SP']
+		response_post = curr_post['RP']
+
+		seeker_post = seeker_post.strip().lower()
+		response_post = response_post.strip().lower()
+
+		if seeker_post.strip() == "my job is becoming more and more stressful with each passing day." and curr_response_orig.strip() == "don't worry! i'm there for you.":
+			output_response_li_op = []
+
+			output_response_li_op.append({'operation': '<REPLACE>', 'sentence': "It must be a real struggle!", 'old_sentence': "Don't worry!"})
+			output_response_li_op.append({'operation': '<NOOP>', 'sentence': "I'm there for you."})
+			output_response_li_op.append({'operation': '<INSERT>', 'sentence': 'Have you tried talking to your boss?'})
+		
+		return jsonify({'op': output_response_li_op})
+
+app.run(debug=True, host="localhost", port=10000)
