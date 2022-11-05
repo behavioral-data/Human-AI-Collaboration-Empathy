407cc19ca2e8b1662423446a1abbbac9f8ca7e9d
ash-shar <ashshar@cs.washington.edu>
2022-11-05 14:10:59 -0700

    removed django code

--- hailey/feedback_interface/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FeedbackInterfaceConfig(AppConfig):
-    name = 'feedback_interface'
--- hailey/feedback_interface/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-# from .views import SignUp
-from .views import index
-
-
-urlpatterns = [
-	path('index.html', index, name='index'),
-]
